f6149ed74faf4ca2bb0d114f604fb19c2a63f3d1
Chris James <quii@Chriss-Mac-mini.local>
2021-06-02 12:12:17 +0100

    tidy up tests a bit

--- blog_test.go
+++ blog_test.go
@@ -1,11 +1,14 @@
 package fstest_spike
 
 import (
+	"github.com/matryer/is"
 	"testing"
 	"testing/fstest"
 )
 
 func TestBlog(t *testing.T) {
+	is := is.New(t)
+	
 	t.Run("it returns a list of blogs from a directory", func(t *testing.T) {
 
 		dirFS := fstest.MapFS{
@@ -14,27 +17,15 @@ func TestBlog(t *testing.T) {
 		}
 
 		posts, err := New(dirFS)
-
-		if err != nil {
-			t.Fatal(err)
-		}
+		is.NoErr(err)
 
 		t.Run("it gets all the files from the directory", func(t *testing.T) {
-			got := len(posts)
-			want := len(dirFS)
-			if got != want {
-				t.Errorf("expected %d posts but got %d", want, got)
-			}
+			is.Equal(len(posts), len(dirFS))
 		})
 
 		t.Run("it parses the first post correctly", func(t *testing.T) {
-			if posts[0].Name != "hello-world.md" {
-				t.Errorf("got %q, want %q", posts[0].Name, "hello-world.md")
-			}
-
-			if posts[0].Body != "hello, world" {
-				t.Errorf("got %q, want %q", posts[0].Body, "hello, world")
-			}
+			is.Equal(posts[0].Name, "hello-world.md")
+			is.Equal(posts[0].Body, "hello, world")
 		})
 
 	})
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/quii/fstest-spike
 
 go 1.16
+
+require github.com/matryer/is v1.4.0
