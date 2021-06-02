a1d3daf69669c8da36dadc546f059f2ae55dc362
Chris James <quii@Chriss-Mac-mini.local>
2021-06-02 13:45:39 +0100

    better structure in the test

--- blog_test.go
+++ blog_test.go
@@ -9,24 +9,20 @@ import (
 func TestBlog(t *testing.T) {
 	is := is.New(t)
 
-	t.Run("it returns a list of blogs from a directory", func(t *testing.T) {
+	dirFS := fstest.MapFS{
+		"hello-world.md":  {Data: []byte("hello, world")},
+		"hello-world.md2": {Data: []byte("hello, world again")},
+	}
 
-		dirFS := fstest.MapFS{
-			"hello-world.md":  {Data: []byte("hello, world")},
-			"hello-world.md2": {Data: []byte("hello, world again")},
-		}
+	posts, err := New(dirFS)
+	is.NoErr(err)
 
-		posts, err := New(dirFS)
-		is.NoErr(err)
+	t.Run("it returns a post for each (valid) file", func(t *testing.T) {
+		is.Equal(len(posts), len(dirFS))
+	})
 
-		t.Run("it gets all the files from the directory", func(t *testing.T) {
-			is.Equal(len(posts), len(dirFS))
-		})
-
-		t.Run("it parses the first post correctly", func(t *testing.T) {
-			is.Equal(posts[0].Name, "hello-world.md")
-			is.Equal(posts[0].Body, "hello, world")
-		})
-
+	t.Run("it parses the first post correctly", func(t *testing.T) {
+		is.Equal(posts[0].Name, "hello-world.md")
+		is.Equal(posts[0].Body, "hello, world")
 	})
 }
